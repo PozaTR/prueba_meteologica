e5bdfaddf91ab33764158efad24a1f2680c2ae20
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-11-06 13:57:44 +0100

    add styles in chartInfo component, create mixin section-title

--- src/components/ChartInfo.js
+++ src/components/ChartInfo.js
@@ -8,42 +8,42 @@ const data = {
   labels: ['00:00:00', '00:00:05', '00:00:10', '00:00:15', '00:00:20', '00:00:25', '00:00:30'],
   datasets: [
     {
-      label: 'power',
+      label: 'Power',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(63, 81, 181, 1)',
+      borderColor: 'rgba(63, 81, 181, 1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
+      pointBorderColor: 'rgba(238, 238, 238, 1)',
+      pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+      pointBorderWidth: 5,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBackgroundColor: 'rgba(0, 33, 41, 1)',
+      pointHoverBorderColor: 'rgba(0, 33, 41, 1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
       data: [65, 60, 55, 50, 45, 40, 45]
     },
     {
-      label: 'temperature',
+      label: 'Temperature',
       fill: false,
       lineTension: 0.5,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(0, 204, 118, 1)',
+      borderColor: 'rgba(0, 204, 118, 1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.5,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
+      pointBorderColor: 'rgba(238, 238, 238, 1)',
+      pointBackgroundColor:'rgba(0, 0, 0, 1)',
+      pointBorderWidth: 5,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBackgroundColor: 'rgba(0, 150, 136, 1)',
+      pointHoverBorderColor: 'rgba(0, 150, 136, 1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
@@ -55,9 +55,10 @@ const data = {
 class ChartInfo extends React.Component {
   render() {
     return(
-      <div className="chartInfo__container">
+      <section className="chartInfo__container">
+        <h2 className="chartInfo__title">Up date information</h2>
         <Line className="chartInfo__data" data={data}/>
-      </div>
+      </section>
   )
   }
 }
