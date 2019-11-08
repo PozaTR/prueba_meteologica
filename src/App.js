7f2572f1fcdd8466605a84f201ffff24ccec6e98
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-11-08 21:26:47 +0100

    add mobile styles in ChartInfo component

--- src/App.js
+++ src/App.js
@@ -58,9 +58,9 @@ class App extends React.Component {
             <Switch>
               <Route exact path='/'>
                 <LiveInfo 
-                  powerInfo={powerInfo.slice(-30)} 
-                  temperatureInfo={temperatureInfo.slice(-30)} 
-                  time={time.slice(-30)} 
+                  powerInfo={powerInfo.slice(-12)} 
+                  temperatureInfo={temperatureInfo.slice(-12)} 
+                  time={time.slice(-12)} 
                   lastPowerInfo={lastPowerInfo} 
                   lastTemperatureInfo={lastTemperatureInfo}
                 />
--- src/components/ChartInfo.js
+++ src/components/ChartInfo.js
@@ -16,22 +16,22 @@ class ChartInfo extends React.Component {
         datasets: [
           {
             label: 'Power',
-            fill: false,
+            fill: true,
             lineTension: 0.1,
-            backgroundColor: 'rgba(63, 81, 181, 1)',
-            borderColor: 'rgba(63, 81, 181, 1)',
+            backgroundColor: 'rgba(85, 162, 181, 0.6)',
+            borderColor: '#3f51b5',
             borderCapStyle: 'butt',
             borderDash: [],
             borderDashOffset: 0.0,
             borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(238, 238, 238, 1)',
-            pointBackgroundColor: 'rgba(0, 0, 0, 1)',
-            pointBorderWidth: 5,
+            pointBorderColor: '#002129',
+            pointBackgroundColor: '#002129',
+            pointBorderWidth: 0,
             pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(0, 33, 41, 1)',
-            pointHoverBorderColor: 'rgba(0, 33, 41, 1)',
+            pointHoverBackgroundColor: '#55a2b5',
+            pointHoverBorderColor: '#002129',
             pointHoverBorderWidth: 2,
-            pointRadius: 1,
+            pointRadius: 3,
             pointHitRadius: 10,
             data: []
           }
@@ -42,22 +42,22 @@ class ChartInfo extends React.Component {
         datasets: [
           {
             label: 'Temperature',
-            fill: false,
+            fill: true,
             lineTension: 0.5,
-            backgroundColor: 'rgba(0, 204, 118, 1)',
-            borderColor: 'rgba(0, 204, 118, 1)',
+            backgroundColor: 'rgba(204, 244, 227, 0.6)',
+            borderColor: '#00cc76',
             borderCapStyle: 'butt',
             borderDash: [],
-            borderDashOffset: 0.5,
+            borderDashOffset: 0.0,
             borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(238, 238, 238, 1)',
-            pointBackgroundColor:'rgba(0, 0, 0, 1)',
-            pointBorderWidth: 5,
+            pointBorderColor: '#009688',
+            pointBackgroundColor:'#009688',
+            pointBorderWidth: 0,
             pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(0, 150, 136, 1)',
-            pointHoverBorderColor: 'rgba(0, 150, 136, 1)',
+            pointHoverBackgroundColor: '#9de6c8',
+            pointHoverBorderColor: '#009688',
             pointHoverBorderWidth: 2,
-            pointRadius: 1,
+            pointRadius: 3,
             pointHitRadius: 10,
             data: []
           }
@@ -93,24 +93,52 @@ class ChartInfo extends React.Component {
     const { powerChartData,  temperatureChartData } = this.state;
 
     return(
-      <section className="chartInfo__container">
+      <section className="chartInfo">
         <h2 className="chartInfo__title">Live stream information</h2>
-        <div className="chartInfo__wrapper">
-          <h3>Power</h3>
+        <div className="chartInfo__wrapper wrapper">
+          <h3 className="chartInfo__chart-name">Power</h3>
           <Line className="chartInfo__data"
             data={powerChartData}
             width={100}
             height={50}
             legend={{display: false}}
+            options={{
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          fontSize: 10
+                      }
+                  }],
+                  xAxes: [{
+                    ticks: {
+                        fontSize: 10
+                    }
+                }]
+              }
+            }}
           />
         </div>
-       <div className="chartInfo__wrapper">
-          <h3>Temperature</h3>
+       <div className="chartInfo__wrapper wrapper">
+          <h3 className="chartInfo__chart-name">Temperature</h3>
           <Line className="chartInfo__data"
             data={temperatureChartData}
             width={100}
             height={50}
             legend={{display: false}}
+            options={{
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          fontSize: 10
+                      }
+                  }],
+                  xAxes: [{
+                    ticks: {
+                        fontSize: 10
+                    }
+                }]
+              }
+            }}
           />  
        </div>
        
