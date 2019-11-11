0c1cb2f1db220cf5941ad025ef7a15fd235c35e4
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-11-11 15:27:55 +0100

    publish project

---
+++ docs/precache-manifest.1fcbb650ec71883a060b06a55dbe877c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c66047a8b6f9ad393ddd062aa38109",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a21a0f9f55529345cd9b",
+    "url": "./static/css/main.49ae5434.chunk.css"
+  },
+  {
+    "revision": "76bacf31838d47facc0f",
+    "url": "./static/js/2.6c28d8e3.chunk.js"
+  },
+  {
+    "revision": "a21a0f9f55529345cd9b",
+    "url": "./static/js/main.410840da.chunk.js"
+  },
+  {
+    "revision": "970f67ea81b508d03b3a",
+    "url": "./static/js/runtime-main.72191255.js"
+  }
+]);
